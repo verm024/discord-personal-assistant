84fc541f167cdc4be0454d25516302268dc55790
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2021-09-29 17:09:46 +0700

    Hotfix: Fix directory path for fonts

--- modules/rquiz.py
+++ modules/rquiz.py
@@ -107,10 +107,12 @@ async def start(message, data, data_key, index=0, length=10, max_try=3):
 
 async def text_to_image(message, text, number):
     img = Image.new("RGB", (900, 250), color=(255, 255, 255))
-    fnt = ImageFont.truetype('../fonts/meiryo.ttc', 128)
+    fnt = ImageFont.truetype(os.path.abspath(
+        os.path.join((os.getcwd()), './fonts/meiryo.ttc')), 128)
     d = ImageDraw.Draw(img)
     d.text((30, 30), "{num}. {kanji}".format(
         num=number, kanji=text), font=fnt, fill=(0, 0, 0))
-    filedir = os.path.abspath(os.getcwd()) + "\modules\cache\\rquiz-temp.png"
+    filedir = os.path.abspath(os.path.join(
+        os.getcwd(), "./modules/cache/rquiz-temp.png"))
     img.save(filedir)
     await message.channel.send(file=discord.File(filedir))
