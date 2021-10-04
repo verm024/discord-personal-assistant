136659ae538e895616f3be7b1582b54ed7a8f40d
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2021-10-04 20:02:02 +0700

    Add meaning quiz feature

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import discord
 import os
 import sys
 from dotenv import load_dotenv
+
+# Modules
 import modules.rquiz as rquiz
+import modules.mquiz as mquiz
 
 
 sys.stdout.reconfigure(encoding='utf-8')
@@ -33,7 +36,15 @@ async def on_message(message):
             data.pop(data_key)
             await message.channel.send("Exited from current activity.")
             return
+        
+        # Reading quiz
         if data[data_key].get_type() == "rquiz":
+            finished = await data[data_key].resume(message)
+            if finished:
+                data.pop(data_key)
+        
+        # Meaning quiz
+        if data[data_key].get_type() == "mquiz":
             finished = await data[data_key].resume(message)
             if finished:
                 data.pop(data_key)
@@ -55,6 +66,21 @@ async def on_message(message):
                 except ValueError as e:
                     print(str(e))
                     return
+
+            # Meaning quiz
+            if second_message == "mq" or second_message == "mquiz":
+                try:
+                    index = int(message_list[2] if len(
+                        message_list) > 2 else "0")
+                    length = int(message_list[3] if len(
+                        message_list) > 3 else "10")
+                    max_try = int(message_list[4] if len(
+                        message_list) > 4 else "3")
+                    await mquiz.start(message, data, data_key, index, length, max_try)
+                except ValueError as e:
+                    print(str(e))
+                    return
+
         elif message.content.lower() == "0pri" or message.content.lower() == "0p":
             await message.channel.send(get_help())
 
